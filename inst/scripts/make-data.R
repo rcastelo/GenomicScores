1a981fb2a57cb30fdc1c79687d561c246ec2d8ac
[rcastelo] <[robert.castelo@upf.edu]>
2016-11-07 18:43:48 +0100

    Fixed export in NAMESPACE. Updated make-data.R to add metadata to Rle-Raw objects.

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -22,6 +22,8 @@ library(BSgenome.Hsapiens.UCSC.hg38)
 library(rtracklayer)
 library(doParallel)
 
+downloadURL <- "http://hgdownload.soe.ucsc.edu/goldenPath/hg38/phastCons100way/hg38.100way.phastCons"
+
 registerDoParallel(cores=4) ## each process may need up to 20Gb of RAM
 
 ## transform WIG to BIGWIG format
@@ -30,6 +32,21 @@ foreach (chr=seqnames(Hsapiens)) %dopar% {
   cat(chr, "\n")
   wigToBigWig(file.path("hg38.100way.phastCons", sprintf("%s.phastCons100way.wigFix.gz", chr)), seqinfo=si)
 }
+
+## freeze the GenomeDescription data for Hsapiens
+
+refgenomeGD <- GenomeDescription(organism=organism(Hsapiens),
+                                 common_name=commonName(Hsapiens),
+                                 provider=provider(Hsapiens),
+                                 provider_version=providerVersion(Hsapiens),
+                                 release_date=releaseDate(Hsapiens),
+                                 release_name=releaseName(Hsapiens),
+                                 seqinfo=Seqinfo(seqnames=seqnames(Hsapiens),
+                                                 seqlengths=seqlengths(Hsapiens),
+                                                 isCircular=isCircular(Hsapiens),
+                                                 genome=releaseName(Hsapiens)))
+
+saveRDS(refgenomeGD, file=file.path("hg38.100way.phastCons", "refgenomeGD.rds"))
 
 ## transform BIGWIG into Rle objects coercing phastCons scores into
 ## 1-decimal digit raw-encoded values to reduce memory requirements
@@ -45,6 +62,12 @@ foreach (chr=seqnames(Hsapiens)) %dopar% {
     assign(sprintf("phastCons100way_%s", chr),
            do.call("runValue<-", list(get(sprintf("phastCons100way_%s", chr)),
                                       as.raw(runValue(get(sprintf("phastCons100way_%s", chr)))))))
+    metadata(get(sprintf("phastCons100way_%s", chr))) <- list(provider="UCSC",
+                                                              provider_version="11May2015", ## it'd better to grab the date from remove file
+                                                              download_url=downloadURL,
+                                                              download_date=format(Sys.Date(), "%b %d, %Y"),
+                                                              reference_genome=refgenomeGD,
+                                                              data_pkgname="phastCons100way.UCSC.hg38")
     saveRDS(get(sprintf("phastCons100way_%s", chr)),
             file=file.path("hg38.100way.phastCons", sprintf("phastCons100way.UCSC.hg38.%s.rds", chr)))
     rm(list=sprintf("phastCons100way_%s", chr))
@@ -53,18 +76,3 @@ foreach (chr=seqnames(Hsapiens)) %dopar% {
       message(chr, " ", conditionMessage(err), call.=TRUE)
   })
 }
-
-## freeze the GenomeDescription data for Hsapiens for later retrieval at loading time
-
-refgenomeGD <- GenomeDescription(organism=organism(Hsapiens),
-                                 common_name=commonName(Hsapiens),
-                                 provider=provider(Hsapiens),
-                                 provider_version=providerVersion(Hsapiens),
-                                 release_date=releaseDate(Hsapiens),
-                                 release_name=releaseName(Hsapiens),
-                                 seqinfo=Seqinfo(seqnames=seqnames(Hsapiens),
-                                                 seqlengths=seqlengths(Hsapiens),
-                                                 isCircular=isCircular(Hsapiens),
-                                                 genome=releaseName(Hsapiens)))
-
-saveRDS(refgenomeGD, file=file.path("hg38.100way.phastCons", "refgenomeGD.rds"))
