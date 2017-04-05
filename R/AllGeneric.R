d160b5401ccafb8dc0c57111fd2d1353268194ed
[rcastelo] <[robert.castelo@upf.edu]>
2017-04-05 17:13:22 +0200

    Fixed the missing scores.only=FALSE argument in the generic of the scores() method.

--- R/AllGeneric.R
+++ R/AllGeneric.R
@@ -1,4 +1,4 @@
 ## for GScores class
-setGeneric("scores", function(object, ranges, scores.only, ...) standardGeneric("scores"))
+setGeneric("scores", function(object, ranges, scores.only=FALSE, ...) standardGeneric("scores"))
 setGeneric("qfun", function(object) standardGeneric("qfun"))
 setGeneric("dqfun", function(object) standardGeneric("dqfun"))
