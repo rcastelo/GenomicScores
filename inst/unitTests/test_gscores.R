ae115037352b49b3f98d165669262af1ccac3300
Robert Castelo <robert.castelo@upf.edu>
2025-02-01 11:07:35 +0100

    Fixed unit tests

--- inst/unitTests/test_gscores.R
+++ inst/unitTests/test_gscores.R
@@ -1,15 +1,15 @@
 test_gscores <- function() {
-  if (require(phastCons100way.UCSC.hg19)) {
+  if (require(phastCons100way.UCSC.hg38)) {
     gr1 <- GRanges(seqnames="chr7", IRanges(start=117232380, width=5))
 
-    gsco <- phastCons100way.UCSC.hg19
+    gsco <- phastCons100way.UCSC.hg38
     res <- gscores(gsco, gr1)
     res$default <- round(res$default, digits=2)
 
     theoreticalres <- gr1
     seqlevels(theoreticalres) <- seqlevels(gsco)
     seqinfo(theoreticalres) <- seqinfo(gsco)
-    theoreticalres$default <- 0.92
+    theoreticalres$default <- 0.48
 
     checkIdentical(res, theoreticalres)
   }
