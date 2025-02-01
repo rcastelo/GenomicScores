233946f6cb84b30b87d00a40cf6b0150f0d2526d
Robert Castelo <robert.castelo@upf.edu>
2025-02-01 11:07:35 +0100

    Fixed unit tests

--- inst/unitTests/test_populations.R
+++ inst/unitTests/test_populations.R
@@ -1,8 +1,12 @@
 test_populations <- function() {
-  if (require(phastCons100way.UCSC.hg19)) {
-    gsco <- phastCons100way.UCSC.hg19
+  if (require(phastCons100way.UCSC.hg38)) {
+    gsco <- phastCons100way.UCSC.hg38
+
     pops <- populations(gsco)
 
     checkIdentical(pops, c("default", "DP2"))
+
+    checkIdentical(unique(genome(gsco)), "Genome Reference Consortium GRCh38")
+    checkIdentical(provider(gsco), "UCSC")
   }
 }
